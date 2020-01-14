4147c2263ce3360d3ca378fe01c9ca8a393172ed
patrickLabes <patrick.labes@digistorm.com.au>
2020-01-15 08:33:36 +1000

    learning about asynData

--- learning/pages/posts/index.vue
+++ learning/pages/posts/index.vue
@@ -17,15 +17,37 @@ export default {
       posts: []
     };
   },
-  mounted() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos")
+  asyncData(context) {
+    // nuxt this is used to do server side redering and front end fetching
+    // console.log(context);
+    return axios.get("https://jsonplaceholder.typicode.com/todos")
       .then(response => {
-        this.posts = response.data;
+        // console.log({posts: response.data})
+        return {posts: response.data}
       })
       .catch(error => {
         console.log(error);
       });
   }
 };
+
+// vue way only render on the front end
+// import axios from "axios";
+// export default {
+//   data() {
+//     return {
+//       posts: []
+//     };
+//   },
+//   mounted() {
+//     axios
+//       .get("https://jsonplaceholder.typicode.com/todos")
+//       .then(response => {
+//         this.posts = response.data;
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   }
+// };
 </script>
